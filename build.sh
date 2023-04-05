30d4074a6321d06839de7e82221868621d94671c
nuanxinqing <nuanxinqing@gmail.com>
2023-04-05 21:36:53 +0800

    update: 静态资源更新

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
+# 编译前；一定需要打开Docker Desktop，否则会报错
+# 编译前；一定需要打开Docker Desktop，否则会报错
+# 编译前；一定需要打开Docker Desktop，否则会报错
+
 # 编译前端文件
+cd /Users/nuanxinqing/Code/Vue/arco-lddp-vue
+yarn build
+
+# 返回项目目录
+cd /Users/nuanxinqing/Code/Golang/LDDP
+
+# 删除现有前端文件
+rm -rf static/assets/*
+
+# 复制前端文件到项目目录
+cp -r /Users/nuanxinqing/Code/Vue/arco-lddp-vue/dist/* static/assets/
+
+# 打包前端文件
 cd static
 go-bindata -o=bindata/bindata.go -pkg=bindata ./assets/...
 cd ..
