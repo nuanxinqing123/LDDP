3cf026a8f05d16df960117882e71218bd1b84e14
Nuanxinqing <nuanxinqing@gmail.com>
2024-11-07 11:56:39 +0800

    docs: update

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 # 编译前；一定需要打开Docker Desktop，否则会报错
 
 # 编译前端文件
-cd /Users/nuanxinqing/Code/Vue/arco-lddp-vue
+cd /Users/nuanxinqing/Code/Vue/arco-lddp-vue || exit
 yarn build
 
 # 返回项目目录
-cd /Users/nuanxinqing/Code/Golang/LDDP
+cd /Users/nuanxinqing/Code/Golang/LDDP || exit
 
 # 删除现有前端文件
 rm -rf static/assets/*
@@ -16,9 +16,12 @@ rm -rf static/assets/*
 cp -r /Users/nuanxinqing/Code/Vue/arco-lddp-vue/dist/* static/assets/
 
 # 打包前端文件
-cd static
+cd static || exit
 go-bindata -o=bindata/bindata.go -pkg=bindata ./assets/...
 cd ..
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o LDDP-windows-amd64 -ldflags '-s -w -extldflags "-static"'
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o LDDP-linux-amd64.exe -ldflags '-s -w -extldflags "-static"'
 
 # 压缩打包文件
 upx LDDP-*
